5e732fb4208255b325b5ee5a7d22adb6d4dd6b7a
BiaLanser <biaalperes@gmail.com>
2023-09-04 14:34:24 -0300

    ok

--- proj0409/ex000.py
+++ proj0409/ex000.py
@@ -1,5 +1,11 @@
 from mydefs import name
 from env import API_KEY
 from mydefs import entra21
+from mydefs import math
 
-print(name, '\n'+ entra21('aaa')) # 'aaa' é argumento de word(mydefs)
+def main():
+    print(name, '\n'+ entra21('aaa')) # 'aaa' é argumento de word(mydefs)
+    print(math(5, 6))
+   
+if __name__ == '__main__': #padrão de main
+    main()
--- proj0409/mydefs.py
+++ proj0409/mydefs.py
@@ -3,3 +3,6 @@ name = "Bia"
 def entra21(word): #word é parâmetro
     ret = str(word)
     return ret.upper()
+
+def math(n1, n2):
+    return n1 * n2
