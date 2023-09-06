1bcd53a639599d5b3133bf5b90035a0ef60a03a4
pcadmin <zuchii.victor@gmail.com>
2023-09-06 14:20:19 -0300

    ok

---
+++ proj0609/ex010.py
@@ -0,0 +1,10 @@
+""""
+Mostre o tamanho da lista de nomes, o número de 
+elementos da lista de nomes e, separadamente, 
+apenas o terceiro elemento desta lista.
+"""
+
+lista = ["Bia", "Cris", "Alex", "Lia", "Leo"]
+print(lista)
+print(len(lista))
+print(lista[2])
