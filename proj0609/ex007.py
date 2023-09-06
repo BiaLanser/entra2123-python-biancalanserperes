361c8790caefb37beb7d35135e2d6a6f917079c3
pcadmin <zuchii.victor@gmail.com>
2023-09-06 14:14:47 -0300

    ok

--- proj0609/ex007.py
+++ proj0609/ex007.py
@@ -2,3 +2,6 @@
 Peça para que o usuário digite um número e, em seguida,
 exiba em tela o número digitado. Utilize print(f).
 """
+
+num = int(input("Digite um número: "))
+print(f"O número digitado é {num}")
