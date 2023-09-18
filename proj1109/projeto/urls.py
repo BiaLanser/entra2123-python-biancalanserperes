67fb899c4016ff08864bbe62865b2fef3fc67487
bia <biaalperes@gmail.com>
2023-09-18 16:16:04 -0300

    ok

--- proj1109/projeto/urls.py
+++ proj1109/projeto/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('angeline.urls')),
+    path('bia/', include('bianca.urls')),  
 ]
